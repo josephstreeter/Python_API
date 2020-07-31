b6091559951d5747f71d045a5a20d0ef65dfb516
Streeter, Joseph <joseph.streeter76@gmail.com>
2020-07-31 15:23:59 -0500

    Updated POST

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class user:
 
 
 @app.route('/api/v1/users', methods=['GET'])
-def new_user():
+def get_user():
     newuser = user(request.args['firstname'],request.args['lastname'])
     
     results={
@@ -23,6 +23,12 @@ def new_user():
         "userid" : newuser.userid
         }
 
+    return jsonify(results)
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    results = request.get_json()
+    print(results)
     return jsonify(results)
 
 app.run()
